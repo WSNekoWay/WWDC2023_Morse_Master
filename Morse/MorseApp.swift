3f6b91c656aa91a9d39ef3dc720a1f0f980ad747
WSNekoWay <banthony03@student.ciputra.ac.id>
2023-05-16 23:06:26 +0700

    Initial Commit

---
+++ Morse/MorseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorseApp.swift
+//  Morse
+//
+//  Created by MacBook Pro on 16/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MorseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
